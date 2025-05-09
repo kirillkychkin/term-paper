b79283dd02abd9518465b66c8e98c98c955e2dc1
Kirill Kychkin <kychkin.kirill@gmail.com>
2025-05-09 18:48:58 +0900

    insert tag categories

--- sql_tag_categs.py
+++ sql_tag_categs.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import mysql.connector
+import json
 import os
 load_dotenv()
 DB_PASSWORD = os.getenv('DB_PASSWORD')
@@ -8,26 +9,46 @@ DB_USER = os.getenv('DB_USER')
 DB_PORT = os.getenv('DB_PORT')
 DB_DATABASE = os.getenv('DB_DATABASE')
 
-try:
-    cnx = mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        port=DB_PORT,
-        password=DB_PASSWORD,
-        database=DB_DATABASE
-    )
+def read_json_file(filename="tags_keywords.json",):
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
 
-    cursor = cnx.cursor()
-    query = "SELECT * FROM tag_categories"
-    cursor.execute(query)
-    
-    results = cursor.fetchall()
-    print(results)
+def insert_tag_categories(data):
+    tags = []
+    for tag in data:
+        insert_tag = (tag, )
+        tags.append(insert_tag)
+    try:
+        cnx = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            port=DB_PORT,
+            password=DB_PASSWORD,
+            database=DB_DATABASE
+        )
 
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
-finally:
-    if 'cursor' in locals() and cursor is not None:
-        cursor.close()
-    if 'cnx' in locals() and cnx is not None:
-        cnx.close()
+        cursor = cnx.cursor()
+        query_insert = "INSERT INTO tag_categories (name) VALUES (%s)"
+
+        cursor.executemany(query_insert, tags)
+        cnx.commit()
+        
+        results = cursor.fetchall()
+        print(results)
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        if 'cursor' in locals() and cursor is not None:
+            cursor.close()
+        if 'cnx' in locals() and cnx is not None:
+            cnx.close()
+
+
+def main():
+    data = read_json_file()
+    insert_tag_categories(data)
+
+if __name__ == "__main__":
+    main()
