534cab3f2828858ca704ec0121d7301c9ac24fde
Kirill Kychkin <kychkin.kirill@gmail.com>
2025-05-09 18:34:43 +0900

    add query, error handling

--- sql_tag_categs.py
+++ sql_tag_categs.py
@@ -8,13 +8,26 @@ DB_USER = os.getenv('DB_USER')
 DB_PORT = os.getenv('DB_PORT')
 DB_DATABASE = os.getenv('DB_DATABASE')
 
-mydb = mysql.connector.connect(
-  host=DB_HOST,
-  user=DB_USER,
-  port=DB_PORT,
-  password=DB_PASSWORD,
-  database=DB_DATABASE
-)
+try:
+    cnx = mysql.connector.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        port=DB_PORT,
+        password=DB_PASSWORD,
+        database=DB_DATABASE
+    )
 
-print(mydb)
-mydb.close()
+    cursor = cnx.cursor()
+    query = "SELECT * FROM tag_categories"
+    cursor.execute(query)
+    
+    results = cursor.fetchall()
+    print(results)
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+    if 'cursor' in locals() and cursor is not None:
+        cursor.close()
+    if 'cnx' in locals() and cnx is not None:
+        cnx.close()
